3326a81d363718682694dadd2693774c10d68eb5
Thakisis <sub722@gmail.com>
2024-02-11 16:09:19 +0100

    added not found

---
+++ app/gallery/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import styles from "../page.module.css"
+import { MainHome } from "@/components/MainHome"
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        gallery
+        <MainHome></MainHome>
+      </div>
+    </main>
+  )
+}
---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+
+import styles from "./page.module.css"
+function Custom404(props) {
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                nada que ver aqui
+            </div>
+        </main>
+    )
+}
+
+export default Custom404
